02b3300a8adc388f7ff7a284626a0a4186ad7012
Олеся Бездомникова <lesia.bezdoma@mail.ru>
2026-02-25 20:24:03 +0300

    Add files via upload

---
+++ projects_2/task_2_2/task_2-2_3.py
@@ -0,0 +1,6 @@
+device_name = "puffmi"
+quntity_of_puff = 9000
+taste = "watermelon ice"
+colour = "red"
+
+print(f"Критерии\t\tДанные\nНазвание прибра\t\t{device_name}\nТяговая способность\t{quntity_of_puff}\nВкус\t\t\t{taste}\nЦвет\t\t\t{colour}")
---
+++ projects_2/task_2_2/task_2-2_inventory_control.py
@@ -0,0 +1,6 @@
+user_input_reactive = input("Название нового реактива: ")
+quantity = input("Количество реактива: ")
+clean_reactive = user_input_reactive.upper()
+
+print(f"Реактив {clean_reactive} поступил на склад в количестве {quantity} шт.")
+
