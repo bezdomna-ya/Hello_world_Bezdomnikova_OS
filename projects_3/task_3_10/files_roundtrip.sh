08af934c881167e11211978693d9152b7c0d1b74
Олеся Бездомникова <lesia.bezdoma@mail.ru>
2026-03-12 19:25:13 +0300

    Add files via upload

---
+++ projects_3/task_3_10/files_roundtrip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in {1..10}; do
+	touch "test$i.txt"
+	echo "Добавлен test$i.txt"
+done
+
+i=10
+
+while [ $i -ge 1 ]; do
+	rm "test$i.txt"
+	echo "Удален test$i.txt"
+	i=$((i - 1))
+done
---
+++ projects_3/task_3_10/odd_filter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {1..20}; do
+	if [ $((i % 2)) -eq 0 ]; then
+		continue
+	elif ((i == 15)); then
+		break
+	else
+		echo "$i"
+	fi
+done
